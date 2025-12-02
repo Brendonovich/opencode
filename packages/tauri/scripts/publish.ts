499be94a507ae9a7f560d602a506950f5155e9e3
Brendan Allan <brendonovich@outlook.com>
2025-12-02 19:06:21 +0800

    fix linux + remove binaries folder

--- packages/tauri/scripts/publish.ts
+++ packages/tauri/scripts/publish.ts
@@ -10,6 +10,11 @@ const dir = 'src-tauri/target/opencode-binaries';
 
 await $`mkdir -p ${dir}`;
 await $`gh release download --pattern ${sidecarConfig.ocBinary}.${sidecarConfig.assetExt} --repo sst/opencode --skip-existing --dir ${dir}`;
-await $`unzip -o ${dir}/${sidecarConfig.ocBinary}.${sidecarConfig.assetExt} -d ${dir}`;
+
+if(sidecarConfig.assetExt === "tar.gz") {
+  await $`tar -xvzf ${dir}/${sidecarConfig.ocBinary}.${sidecarConfig.assetExt} -C ${dir}`
+} else {
+  await $`unzip -o ${dir}/${sidecarConfig.ocBinary}.${sidecarConfig.assetExt} -d ${dir}`;
+}
 
 await copyBinaryToSidecarFolder(`${dir}/opencode`)
