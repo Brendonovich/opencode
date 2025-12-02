170e7650f2f1c87e0bf5c7c9da58c7fd72978954
Brendan Allan <brendonovich@outlook.com>
2025-12-02 20:37:44 +0800

    no relative?

--- packages/desktop/vite.js
+++ packages/desktop/vite.js
@@ -13,7 +13,7 @@ export default [
       return {
         resolve: {
           alias: {
-            "@": fileURLToPath(new URL("../src", import.meta.url)),
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
           },
         },
       }
