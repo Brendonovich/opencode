291a2ea601133b177ff28c49b35fa1880350efef
Brendan Allan <brendonovich@outlook.com>
2025-12-02 18:59:54 +0800

    more assetExt jfc

--- packages/tauri/scripts/publish.ts
+++ packages/tauri/scripts/publish.ts
@@ -10,6 +10,6 @@ const dir = 'src-tauri/target/opencode-binaries';
 
 await $`mkdir -p ${dir}`;
 await $`gh release download --pattern ${sidecarConfig.ocBinary}.${sidecarConfig.assetExt} --repo sst/opencode --skip-existing --dir ${dir}`;
-await $`unzip -o ${dir}/${sidecarConfig.ocBinary}.zip -d ${dir}`;
+await $`unzip -o ${dir}/${sidecarConfig.ocBinary}.${sidecarConfig.assetExt} -d ${dir}`;
 
 await copyBinaryToSidecarFolder(`${dir}/opencode`)
