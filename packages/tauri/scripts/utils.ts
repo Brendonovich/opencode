c3292405864662c3a6e2c1220cac6eaea3f8de97
Brendan Allan <brendonovich@outlook.com>
2025-12-02 18:58:35 +0800

    -_-

--- packages/tauri/scripts/utils.ts
+++ packages/tauri/scripts/utils.ts
@@ -4,22 +4,22 @@ export const SIDECAR_BINARIES: Array<{ rustTarget: string; ocBinary: string, ass
   {
     rustTarget: "aarch64-apple-darwin",
     ocBinary: "opencode-darwin-arm64",
-    assetExt: ".zip"
+    assetExt: "zip"
   },
   {
     rustTarget: "x86_64-apple-darwin",
     ocBinary: "opencode-darwin-x64",
-    assetExt: ".zip"
+    assetExt: "zip"
   },
   {
     rustTarget: "x86_64-pc-windows-msvc",
     ocBinary: "opencode-windows-x64",
-    assetExt: ".zip"
+    assetExt: "zip"
   },
   {
     rustTarget: "x86_64-unknown-linux-gnu",
     ocBinary: "opencode-linux-x64",
-    assetExt: ".tar.gz"
+    assetExt: "tar.gz"
   }
 ]
 
